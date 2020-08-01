65864502d7fccca2782dbcbfef06082c5479d3db
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-08-01 15:27:48 +0200

    added more fields to institution entity

--- src/main/java/pl/coderslab/charity/entity/Institution.java
+++ src/main/java/pl/coderslab/charity/entity/Institution.java
@@ -13,8 +13,62 @@ public class Institution {
     private String description;
     @OneToMany(mappedBy = "institution")
     private List<Donation> donations;
+    private String aboutUs;
+    private String city;
+    private String street;
+    private String zipCode;
+    private String mail;
+    private String phoneNumber;
 
     public Institution() {
+    }
+
+    public String getAboutUs() {
+        return aboutUs;
+    }
+
+    public void setAboutUs(String aboutUs) {
+        this.aboutUs = aboutUs;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     public List<Donation> getDonations() {
