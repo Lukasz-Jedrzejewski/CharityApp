aecab85a7c4d16dfae06f382a027aac9aef8dc59
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-08-18 11:33:38 +0200

    scrolling to every part of index view

--- src/main/webapp/resources/js/menu.js
+++ src/main/webapp/resources/js/menu.js
@@ -1,9 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     var steps = document.querySelector("#stepsScroll");
-    console.log(steps);
     steps.addEventListener("click", function ()
     {
         document.querySelector(".steps").scrollIntoView();
+    });
+
+    var about = document.querySelector("#aboutScroll");
+    about.addEventListener("click", function ()
+    {
+        document.querySelector(".about-us").scrollIntoView();
+    });
+
+    var help = document.querySelector("#helpScroll");
+    help.addEventListener("click", function ()
+    {
+        document.querySelector(".help").scrollIntoView();
+    });
+
+    var contact = document.querySelector("#contactScroll");
+    contact.addEventListener("click", function ()
+    {
+        document.querySelector(".contact").scrollIntoView();
     });
 });
